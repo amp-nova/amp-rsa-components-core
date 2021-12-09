6e6fd5fb722c6005e8ef3710d26b824c3ed6656c
Réza Kalfane <reza.kalfane@gmail.com>
2021-12-09 11:34:19 +0100

    Added

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Banner } from "./Banner";
+import { Card } from "./Card";
+import { CardList } from "./CardList";
+import { FAQ } from "./FAQ";
+import { Image } from "./Image";
+import { Text } from "./Text";
+import { Video } from "./Video";
+
+export { Banner };
+export { Card };
+export { CardList };
+export { FAQ };
+export { Image };
+export { Text };
+export { Video };
